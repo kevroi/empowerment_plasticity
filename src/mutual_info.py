f367001913c31b972bc3b3d687baa27bf2dd3911
Kevin Roice <kevinroice@outlook.com>
2025-05-27 16:17:08 -0600

    chore: mutual info calculations

--- src/mutual_info.py
+++ src/mutual_info.py
@@ -8,25 +8,31 @@ def entropy(xs: Sequence[Hashable]) -> float:
     probabilities = np.array([count / total for count in counts.values()])
     return -np.sum(p * np.log2(p) for p in probabilities if p > 0)
 
-def joint_entropy(xs: Sequence[Hashable], ys: Sequence[Hashable]) -> float:
+def joint_entropy(xs: Sequence[Hashable],
+                  ys: Sequence[Hashable]) -> float:
     joint_counts = Counter(zip(xs, ys))
     total = len(xs)
     probabilities = np.array([count / total for count in joint_counts.values()])
     return -np.sum(p * np.log2(p) for p in probabilities if p > 0)
 
-def mutual_info(xs: Sequence[Hashable], ys: Sequence[Hashable]) -> float:
+def mutual_info(xs: Sequence[Hashable],
+                ys: Sequence[Hashable]) -> float:
     return entropy(xs) + entropy(ys) - joint_entropy(xs, ys)
 
-def conditional_entropy(xs: Sequence[Hashable], given_ys: Sequence[Hashable]) -> float:
+def conditional_entropy(xs: Sequence[Hashable],
+                        given_ys: Sequence[Hashable]) -> float:
     return joint_entropy(xs, given_ys) - entropy(given_ys)
 
-def conditional_mutual_info(xs: Sequence[Hashable], ys: Sequence[Hashable], given_zs: Sequence[Hashable]) -> float:
+def conditional_mutual_info(xs: Sequence[Hashable],
+                            ys: Sequence[Hashable],
+                            given_zs: Sequence[Hashable]) -> float:
     h_x_given_z = conditional_entropy(xs, given_zs)
     yz_pairs = list(zip(ys, given_zs))
     h_x_given_yz = conditional_entropy(xs, yz_pairs)
     return h_x_given_z - h_x_given_yz
 
-def directed_info(x_seqs: Sequence[Sequence[Hashable]], y_seqs: Sequence[Sequence[Hashable]]) -> float:
+def directed_info(x_seqs: Sequence[Sequence[Hashable]],
+                  y_seqs: Sequence[Sequence[Hashable]]) -> float:
     T = len(x_seqs[0])
     total_di = 0
     for i in range(T):
@@ -40,4 +46,7 @@ def directed_info(x_seqs: Sequence[Sequence[Hashable]], y_seqs: Sequence[Sequenc
 # windowed directed info
 # directed_info / history_length
 # empowerment has a max over DIs in its defintion
-# generalised directed info
+# generalised directed info - this conditions on two sequences
+# try finding examples where empoweremnt and plasticyt should vary in specific ways.
+# Also DI is not symmetric, so yoiu could have some test for that
+# DI can never go down right? Windowed DI may.
