a4f1b8dc21319683f36d12f45b9ae766421edd15
Kevin Roice <kevinroice@outlook.com>
2025-07-25 01:43:45 +0100

    feat: changing goal position in 4rooms

--- src/environments/FourRooms.py
+++ src/environments/FourRooms.py
@@ -7,17 +7,22 @@ class FourRooms(gym.Env):
     metadata = {"render_modes": ["human"],
                 "render_fps": 4}
 
-    def __init__(self, render_mode=None, max_steps=500):
+    def __init__(self,
+                 render_mode=None,
+                 max_steps=500,
+                 goal_index=0):
         super().__init__()
         self.grid_size = 11
         self.max_steps = max_steps
         self.render_mode = render_mode
+        self.goal_positions = [(9, 9), (1, 1), (1, 9), (9, 1)]
+        self.goal_index = goal_index
 
         self.observation_space = spaces.Discrete(self.grid_size * self.grid_size)
         self.action_space = spaces.Discrete(4)  # up, right, down, left
 
         self.agent_pos = None
-        self.goal_pos = (9, 9)
+        self.goal_pos = self.goal_positions[self.goal_index]
         self.steps_taken = 0
 
         self._build_walls()
@@ -42,15 +47,16 @@ class FourRooms(gym.Env):
     def _state_to_pos(self, state):
         return (state // self.grid_size, state % self.grid_size)
 
-    def reset(self, seed=None, options=None):
+    def reset(self, seed=None, options=None, goal_index=0):
         super().reset(seed=seed)
         self.agent_pos = (0, 0)
         self.steps_taken = 0
+        self.goal_index = goal_index
+        self.goal_pos = self.goal_positions[self.goal_index]
         return self._pos_to_state(self.agent_pos), {}
 
     def step(self, action):
         self.steps_taken += 1
-
         move = [(-1, 0), (0, 1), (1, 0), (0, -1)][action]
         next_pos = (self.agent_pos[0] + move[0], self.agent_pos[1] + move[1])
 
@@ -70,7 +76,12 @@ class FourRooms(gym.Env):
     def render(self):
         grid = np.full((self.grid_size, self.grid_size), ".", dtype=str)
         grid[self.walls] = "#"
+        # Mark all possible goal positions with 'o'
+        for pos in self.goal_positions:
+            grid[pos] = "o"
+        # Mark the current goal with 'G' (overwrites 'o' if overlapping)
         grid[self.goal_pos] = "G"
+        # Mark the agent with 'A' (overwrites any symbol if overlapping)
         grid[self.agent_pos] = "A"
         print("\n".join("".join(row) for row in grid))
         print()
